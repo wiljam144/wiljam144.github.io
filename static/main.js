fd36a03dc2c0a22490549682f4e7ebabe42af92c
Wiljam Holmes <wiljam2016@gmail.com>
2023-02-17 20:38:48 +0100

    changed colorscheme to more retro and added cool effect

--- static/main.js
+++ static/main.js
@@ -9,3 +9,33 @@ document.addEventListener('DOMContentLoaded', (_event) => {
     hljs.highlightElement(el);
   });
 });
+
+const letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const originaltext = document.querySelector("h1").innerText; 
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innertText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return originaltext[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+
+    if (iteration >= originaltext.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
