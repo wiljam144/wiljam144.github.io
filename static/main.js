884dbc3a0ffd061c102bcba3f43ff44a9b0cc520
Wiljam Holmes <wiljam2016@gmail.com>
2023-02-17 20:51:20 +0100

    fixed bug

--- static/main.js
+++ static/main.js
@@ -1,3 +1,33 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const origText = document.querySelector(".title").innerText;
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 52)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
 document.addEventListener('DOMContentLoaded', (_event) => {
   document.querySelectorAll('.src').forEach((el) => {
     // sometimes hljs detected lua as visual basic
@@ -9,33 +39,3 @@ document.addEventListener('DOMContentLoaded', (_event) => {
     hljs.highlightElement(el);
   });
 });
-
-const letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
-const originaltext = document.querySelector("h1").innerText; 
-
-let interval = null;
-
-document.querySelector("h1").onmouseover = event => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innertText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return originaltext[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-
-    if (iteration >= originaltext.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 30);
-}
